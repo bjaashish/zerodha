fc6df3c496e9738b23e278559205d56c6f4a34c1
ashish.bhardwaj <ashish.bhardwaj@centilytics.com>
2024-10-17 20:56:55 +0530

    first commit

---
+++ src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Hero from './Hero'
+import Brokerage from './Brokerage'
+import Footer from '../Footer'
+
+const PricingPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Brokerage/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default PricingPage
